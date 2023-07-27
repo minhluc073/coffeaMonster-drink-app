c3863a3f7cecd7403271ad88c82539616df088c8
minhluc073 <nguyenminhluc0703@gmail.com>
2023-07-27 17:05:02 +0700

    up

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -7,3 +7,62 @@ if ($(".drink-swiper").length > 0) {
     loop: true,
   });
 }
+
+if ($(".recomand-swiper").length > 0) {
+  var swiperRecomandSwiper = new Swiper(".recomand-swiper", {
+    speed: 500,
+    parallax: true,
+    slidesPerView: 1.1,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-btn-next",
+      prevEl: ".swiper-btn-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      576: {
+        slidesPerView: 1.5,
+      },
+    },
+  });
+}
+
+if ($(".drink-swiper-2").length > 0) {
+  var swiperRecomandSwiper = new Swiper(".drink-swiper-2", {
+    speed: 500,
+    parallax: true,
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-btn-next",
+      prevEl: ".swiper-btn-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      576: {
+        slidesPerView: 1.5,
+      },
+    },
+  });
+}
