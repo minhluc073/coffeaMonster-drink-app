1bd32f70c2f9375827861d134213a2b1abd1fec1
minhluc073 <nguyenminhluc0703@gmail.com>
2023-07-31 15:59:54 +0700

    up sign in, sign up

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+/*
+ * show pass
+
+ */
+
+(function ($) {
+  "use strict";
+
+  /* show pass
+  ------------------------------------------------------------------------------------- */
+  var showPass = function () {
+    $(".show-pass").on("click", function () {
+      $(this).toggleClass("active");
+      if ($(".password-field").attr("type") == "password") {
+        $(".password-field").attr("type", "text");
+      } else if ($(".password-field").attr("type") == "text") {
+        $(".password-field").attr("type", "password");
+      }
+    });
+    $(".show-pass2").on("click", function () {
+      $(this).toggleClass("active");
+      if ($(".password-field2").attr("type") == "password") {
+        $(".password-field2").attr("type", "text");
+      } else if ($(".password-field2").attr("type") == "text") {
+        $(".password-field2").attr("type", "password");
+      }
+    });
+  };
+  /* image select
+  ------------------------------------------------------------------------------------- */
+
+  var selectImages = function () {
+    if ($(".image-select").length > 0) {
+      const selectIMG = $(".image-select");
+      selectIMG.find("option").each((idx, elem) => {
+        const selectOption = $(elem);
+        const imgURL = selectOption.attr("data-thumbnail");
+        if (imgURL) {
+          selectOption.attr(
+            "data-content",
+            "<img src='%i'/> %s"
+              .replace(/%i/, imgURL)
+              .replace(/%s/, selectOption.text())
+          );
+        }
+      });
+      selectIMG.selectpicker();
+    }
+  };
+
+  $(function () {
+    showPass();
+    selectImages();
+  });
+})(jQuery);
